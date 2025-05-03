bfe7f1dc2e9ff8385b131ebb842943eec66f0743
Hori <66873519+relpia@users.noreply.github.com>
2025-05-03 23:28:43 +0900

    Update PlayerEntityRendererMixin.java

--- src/main/java/org/stellium/ignoring/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/org/stellium/ignoring/mixin/PlayerEntityRendererMixin.java
@@ -1,29 +1,24 @@
 package org.stellium.ignoring.mixin;
 
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.entity.Entity;
 import org.stellium.ignoring.config.IgnoringConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntityRenderer.class)
-public abstract class PlayerEntityRendererMixin extends EntityRenderer<AbstractClientPlayerEntity> {
-
-    protected PlayerEntityRendererMixin(EntityRendererFactory.Context ctx) {
-        super(ctx);
-    }
-
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private void render(AbstractClientPlayerEntity player, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        if (IgnoringConfig.get().ignoredPlayerList.contains(player.getNameForScoreboard()) && (IgnoringConfig.get().ignoreRender)) {
-                ci.cancel();
+@Mixin(EntityRenderer.class)
+public abstract class PlayerEntityRendererMixin{
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private void onShouldRender(Entity entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+        if (entity instanceof AbstractClientPlayerEntity) {
+            AbstractClientPlayerEntity playerEntity = (AbstractClientPlayerEntity) entity;
+            if (IgnoringConfig.get().ignoredPlayerList.contains(playerEntity.getName().getString()) && IgnoringConfig.get().ignoreRender) {
+                cir.setReturnValue(false);
+            }
         }
     }
 }
-
